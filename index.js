a57de8353cf885c12b1d9a4c9b9e59fdbec5ad34
Lara Sanders <larasanders@gmail.com>
2018-06-03 17:22:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function lowerCaseDrivers(drivers) {
-  const newDrivers = map(drivers, function(driver) {
+  const newDrivers = map(drivers, function (driver) {
     return object.assign ({}, driver, {driver.toLowerCase())
 })
 }
-newDrivers;
